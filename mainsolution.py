90410777743037cb32b0b978efc26b3ea32feaa1
kai zeng <S1021731@connect.np.edu.sg>
2023-07-24 15:27:47 +0800

    first commit

---
+++ mainsolution.py
@@ -0,0 +1,18 @@
+print("hello world! this is our team's solution")
+
+def add():
+    print("this function add")
+    return
+
+def delete():
+    print("this function deletes")
+    return
+
+def multiply():
+    print("this function multiply")
+    return
+
+
+add()
+delete()
+multiply()
